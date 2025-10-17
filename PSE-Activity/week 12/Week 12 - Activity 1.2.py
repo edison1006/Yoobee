ebc0c055b4708451504b5e6a155c354dca56abf9
edison1006 <edisonzhang0321@gmail.com>
2025-10-18 09:05:11 +1300

    Create Week 12 - Activity 1.2.py

---
+++ PSE-Activity/week 12/Week 12 - Activity 1.2.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Use /cal/<int:number> to see its square."
+
+@app.route('/cal/<int:number>')
+def show_square(number):
+    return f"The square of {number} is {number**2}"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
